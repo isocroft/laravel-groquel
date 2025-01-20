9d9134a2301cb34b4573e639c7d88e8dde0f34de
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-20 13:41:01 +0100

    Update DatabaseQueryTaskHandler.php

--- src/QueryHandlers/DatabaseQueryTaskHandler.php
+++ src/QueryHandlers/DatabaseQueryTaskHandler.php
@@ -5,7 +5,42 @@ use Groquel\Laravel\QueryHandlers\StorageQueryTaskHanddler;
 use Groquel\Laravel\QueryHandlerTasks\EloquentQueryBuilderTask;
 
 final class DatabaseQueryTaskHandler extends StorageQueryTaskHanddler {
-  
+  /**
+    * @param EloquentQueryBuilderTask $queryTask
+    * @return mixed
+    * @throws Exception
+    */
+  protected function beginProcessing(EloquentQueryBuilderTask $queryTask) {
+    
+  }
+
+  /**
+    * @param EloquentQueryBuilderTask $queryTask
+    * @return void
+    * @throws Exception
+    */
+  protected function finalizeProcessing(EloquentQueryBuilderTask $queryTask, $result): void {
+      
+  }
+
+  /**
+    * @param EloquentQueryBuilderTask $queryTask
+    * @param Exception $error
+    * @return void
+    * @throws Exception
+    */
+  protected function finalizeProcessingWithError(EloquentQueryBuilderTask $queryTask, Exception $error): void {
+      
+  }
+
+  /**
+    * @param EloquentQueryBuilderTask $queryTask
+    * @return mixed
+    * @throws Exception
+    */
+  protected function alternateProcessing(EloquentQueryBuilderTask $queryTask) {
+    
+  }
 }
 
 ?>
