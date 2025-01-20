e44fbf9833436252c23e40b1a46417b245f42d40
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-20 13:35:37 +0100

    Create CacheQueryTaskHandler.php

---
+++ src/QueryHandlers/CacheQueryTaskHandler.php
@@ -0,0 +1,10 @@
+<?php
+namespace Groquel\Laravel\QueryHandlers;
+
+use Groquel\Laravel\QueryHandlers\StorageQueryTaskHanddler
+
+final class CacheQueryTaskHandler extends StorageQueryTaskHanddler {
+  
+}
+
+?>
