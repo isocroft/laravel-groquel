cd7dd47e744b526bda8d4fed14d8b6dd6410ef60
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-20 13:38:00 +0100

    Update DatabaseQueryTaskHandler.php

--- src/QueryHandlers/DatabaseQueryTaskHandler.php
+++ src/QueryHandlers/DatabaseQueryTaskHandler.php
@@ -2,6 +2,7 @@
 namespace Groquel\Laravel\QueryHandlers;
 
 use Groquel\Laravel\QueryHandlers\StorageQueryTaskHanddler;
+use Groquel\Laravel\QueryHandlerTasks\EloquentQueryBuilderTask;
 
 final class DatabaseQueryTaskHandler extends StorageQueryTaskHanddler {
   
