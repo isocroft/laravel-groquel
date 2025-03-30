bc1d31ce5ccc6b1257f4e99c11a8eb748a9936a5
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-03-30 06:37:48 +0100

    Create CoreTest.php

---
+++ tests/Functional/CoreTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Groquel\Laravel\Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+
+class CoreTest extends TestCase {
+    public function testClient(): void {
+        static::assertNull(NULL);
+    }
+}
+
+?>
