a63a7a14005757c824fee60b9174847751b85143
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-03-30 05:12:27 +0100

    Create .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'vendor',
+        'tests/fixtures'
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PhpCsFixer' => true,
+        'align_multiline_comment' => false
+    ])
+    ->setFinder($finder)
