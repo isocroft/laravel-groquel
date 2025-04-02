8d71a85a4d88c29d2c06871e4b7859df6c6b219a
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-04-02 14:53:15 +0100

    Update CacheQueryTaskHandler.php

--- src/QueryHandlers/CacheQueryTaskHandler.php
+++ src/QueryHandlers/CacheQueryTaskHandler.php
@@ -4,14 +4,37 @@ namespace Groquel\Laravel\QueryHandlers;
 use Groquel\Laravel\QueryHandlers\StorageQueryTaskHandler;
 use Groquel\Laravel\QueryHandlerTasks\EloquentQueryBuilderTask;
 
+use Illuminate\Support\Facades\Cache as QueryCache;
+
 final class CacheQueryTaskHandler extends StorageQueryTaskHanddler {
+  /**
+    * @param string $queryKey
+    * @return boolean
+    */
+  private function canQuery(string $queryKey) {
+    return QueryCache::has($queryKey);
+  }
   /**
     * @param EloquentQueryBuilderTask $queryTask
     * @return mixed
     * @throws Exception
     */
   protected function beginProcessing(EloquentQueryBuilderTask $queryTask) {
-    
+    $canProceedWithProcessing = false;
+    $isSQLDatabaseQueryTask = false;
+    $sql = $queryTask->getQuerySql();
+
+    if ($sql !== "") {
+      $canProceedWithProcessing = (strtolower(substr($sql, 0, 6)) === "select" || strtolower(substr($sql, 0, 10)) === "db_select:")
+        && strpos(strtolower($sql), "join") === false;
+      $isSQLDatabaseQueryTask = true
+    }
+
+    if (!canProceedWithProcessing) {
+      return $this->skipHandlerProcessing();
+    }
+
+    /* @TODO: More code here... */
   }
 
   /**
