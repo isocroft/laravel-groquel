02a296964431a3147b3203acbe1e53b13c22f982
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-02 09:44:12 +0100

    chore: cleaned up imports

--- src/GroquelServiceProvider.php
+++ src/GroquelServiceProvider.php
@@ -4,7 +4,8 @@ namespace Groquel\Laravel;
 use Groquel\Laravel\QueryHandlers\CacheQueryTaskHandler;
 use Groquel\Laravel\QueryHandlers\DatabaseQueryTaskHandler;
 
-use Illuminate\Container\Container;
+//use Illuminate\Container\Container;
+//use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 abstract class GroquelServiceProvider extends ServiceProvider
@@ -24,7 +25,7 @@ abstract class GroquelServiceProvider extends ServiceProvider
             "cache: error message to skip processing"
           );
         });
-        $this->app->singleton('QueryHandlersList', function (Container $app) {
+        $this->app->singleton('QueryHandlersList', function (/*Application|Container*/$app) {
           return [$app->make(CacheQueryTaskHandler::class), $app->make(DatabaseQueryTaskHandler::class)];
         });
     }
